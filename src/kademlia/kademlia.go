4e547d60070a062e6d9c67551440d2ed3dabc7fd
Cassandra Rommel <Cassie@vpn-165-124-161-126.vpn.northwestern.edu>
2014-02-17 09:42:40 -0600

    Just making the error a little more clear

--- src/kademlia/kademlia.go
+++ src/kademlia/kademlia.go
@@ -273,11 +273,11 @@ func (kademlia *Kademlia) SendIterativeFindNode(nodeToFind ID) (error, []*Contac
 	shortList := make([]*IterativeContact, 0, const_k) //slice - array with 0 things now and a capacity of const_k
 	//have to do at least one call to kick it off
 	foundContacts := kademlia.RoutingTable.FindKClosestNodes(const_alpha, nodeToFind, kademlia.RoutingTable.SelfContact.NodeID)
-	log.Printf("Found initial %v nodes: ", len(foundContacts))
 
-	if len(foundContacts) == 0 {
-		return nil, nil
-	}
+    if len(foundContacts)<=0 {
+        err := errors.New("No initial nodes found")
+        return err, nil
+    }
 
 	var closestPosition, farthestPosition int = 0, 0
 
@@ -302,13 +302,10 @@ func (kademlia *Kademlia) SendIterativeFindNode(nodeToFind ID) (error, []*Contac
 
 	}
 
-	log.Printf("Looking at the closestPosition:%v for a shortList of len:%v", closestPosition, len(shortList))
-
 	nothingCloser := false
 	triedAll := false
 	for !triedAll && !nothingCloser { //we will keep looping until we hit one of two conditions:
 
-		log.Printf("Beginning of iteration\n")
 		printShortList(shortList, nodeToFind, closestPosition, farthestPosition)
 
 		//there are k active nodes in the short list (tried everything) or nothing returned is closer than before
@@ -336,7 +333,7 @@ func (kademlia *Kademlia) SendIterativeFindNode(nodeToFind ID) (error, []*Contac
 			}
 		}
 		<-timer.C //stop executing until the timer runs out
-		log.Printf("Looking at the closestPosition:%v", closestPosition)
+
 		closestDistance := shortList[closestPosition].contact.NodeID.DistanceBucket(nodeToFind)
 		farthestDistance := shortList[farthestPosition].contact.NodeID.DistanceBucket(nodeToFind)
 		//Collect everything
@@ -395,7 +392,6 @@ func (kademlia *Kademlia) SendIterativeFindNode(nodeToFind ID) (error, []*Contac
 			}
 		}
 
-		log.Printf("Iteration complete\n")
 		printShortList(shortList, nodeToFind, closestPosition, farthestPosition)
 	}
 
